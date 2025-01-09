160d925ca38f722ce4876600ca52be45480474b0
RuslanRad <r140390@yandex.ru>
2025-01-09 18:54:50 +0500

    Spring Data JPA - исправление[18]

--- src/main/java/ru/job4j/bmb/repository/UserRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserRepository.java
@@ -7,6 +7,6 @@ import ru.job4j.bmb.model.User;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findAll();
 }
